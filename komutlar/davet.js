d33b6f0f99db50ac96ff74907d231ec5c2571a3d
BaRoNeaS <41582600+BaRoNeaS@users.noreply.github.com>
2018-11-11 10:16:16 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,28 +1,44 @@
-module.exports = Cmds.addCommand({
-    cmds: [";autoroles"],
+const Discord = require("discord.js");
 
-    requires: {
-        guild: true,
-        loud: false
-    },
+module.exports.run = async (bot, message, args) => {
 
-    desc: "Get all autoroles (roles which users are allowed to assign to themselves)",
+  //!addrole <@user> <Role>
+  if(args[0] == "help"){
+    let helpembxd = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .addField("Addrole Command", "Usage: !addrole <@user> <role>")
 
-    args: "",
+    message.channel.send(helpembxd);
+    return;
+  } 
 
-    example: "",
+  let xdemb = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .setTitle(`Addrole command`)
+  .addField("Description:", "Add role to member", true)
+  .addField("Usage", "!addrole [user] [role]", true)
+  .addField("Example", "!addrole @Odar Member")
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You don't have premmsions to do that!");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.channel.send(xdemb);
 
-    func: (cmd, args, msgObj, speaker, channel, guild) => {
-        var sendEmbedFields = [];
-        var guildAutoRoles = Data.guildGet(guild, Data.autoRoles);
+  let role = args.join(" ").slice(22);
+  if(!role) return message.channel.send("Specify a role!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.channel.send("Couldn't find that role.");
 
-        for (var name in guildAutoRoles) {
-            if (!guildAutoRoles.hasOwnProperty(name)) continue;
-            sendEmbedFields.push({name: name, value: "Role: " + guildAutoRoles[name], inline: false});
-        }
+  if(rMember.roles.has(gRole.id)) return message.channel.send("This user already have that role.");
+  await(rMember.addRole(gRole.id));
 
-        Util.sendEmbed(channel, "AutoRoles", null, Util.makeEmbedFooter(speaker), null, colGreen, sendEmbedFields);
-    }
-});
+    await message.channel.send(`***I just gave ${rMember.user.username} the ${gRole.name} role!***`)
+
+    message.delete();
+  
+}
+
+module.exports.help = {
+  name: "addrole",
+  description: 'Add role to someone',
+  usage: 'addrole <@user> <Role>'
+}
